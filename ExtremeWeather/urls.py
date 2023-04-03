16d286886da792d01ce754c047af5c3f76a4b196
Jose J. Lopez-Perez <97471636+JJWesker10@users.noreply.github.com>
2023-04-03 14:57:21 -0400

    MainScreen.html runs on local Django Machine

--- ExtremeWeather/urls.py
+++ ExtremeWeather/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ew import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.my_view, name='views'),
+    # other URL patterns go here
 ]
--- ew/views.py
+++ ew/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def my_view(request):
+    return render(request, 'WebsiteDesign/MainPage.html')
