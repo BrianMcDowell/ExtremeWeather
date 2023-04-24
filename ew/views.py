838cd6e8fd846e688c537f2be00444c3a2a2830f
BrianMcDowell <brianmcdowell@ufl.edu>
2023-04-24 17:43:02 +0100

    Functioning code for 2 state comparison

--- ew/views.py
+++ ew/views.py
@@ -271,51 +271,80 @@ def states(request):
         "yr": [],
         "data1": [],
         "data2": [],
+        "data3": [],
+        "data4": [],
     }
-    state1 = "None"
-    state2 = "None"
+    state1 = "AK"
+    state2 = "WY"
     if request.method == "POST":
         for key, val in request.POST.items():
             print(key, val)
         state1 = request.POST["state1"]
-    #    state2 = request.POST["state2"]
+        state2 = request.POST["state2"]
         the_chart["d1state"] = json.dumps(state1)
         the_chart["d2state"] = json.dumps(state2)
 
-        query_items = (state1, 'WY')
+        query_items = (state1, state1, state2, state2)
         select_query = """
-            SELECT p.year, total_population, total_events
+            SELECT s1.year, s1.pop, s1.events, s2.pop, s2.events
             FROM
-                (SELECT sum(p.population) as total_population, l.statename, p.year
-                FROM michaelrodelo.populations p
-                INNER JOIN michaelrodelo.location l 
-                ON p.location_id = l.fips
-                WHERE statename = (:1)      
-                GROUP BY l.statename, p.year) p
+                (SELECT p.year AS year, total_population AS pop, events
+                FROM
+                    (SELECT sum(p.population) as total_population, l.statename, p.year
+                    FROM michaelrodelo.populations p
+                    INNER JOIN michaelrodelo.location l 
+                    ON p.location_id = l.fips
+                    WHERE statename = (:1)      
+                    GROUP BY l.statename, p.year) p
+                INNER JOIN
+                    (SELECT statename, year, sum(num) AS events
+                    FROM michaelrodelo.location l 
+                        INNER JOIN 
+                        (SELECT yr.year, location_id, COUNT(event_type) AS num
+                        FROM (SELECT DISTINCT EXTRACT(YEAR FROM event_date) AS year 
+                            FROM michaelrodelo.events) yr 
+                        LEFT JOIN michaelrodelo.events e
+                        ON EXTRACT(YEAR FROM e.event_date)=yr.year
+                        GROUP BY yr.year, location_id) e
+                    ON l.fips = e.location_id
+                    WHERE statename = (:2)     
+                    GROUP BY statename, year) e
+                ON p.year = e.year) s1
             INNER JOIN
-                (SELECT statename, year, sum(num) AS total_events
-                FROM michaelrodelo.location l 
-                    INNER JOIN 
-                    (SELECT yr.year, location_id, COUNT(event_type) AS num
-                    FROM (SELECT DISTINCT EXTRACT(YEAR FROM event_date) AS year 
-                        FROM michaelrodelo.events) yr 
-                    LEFT JOIN michaelrodelo.events e
-                    ON EXTRACT(YEAR FROM e.event_date)=yr.year
-                    GROUP BY yr.year, location_id) e
-                ON l.fips = e.location_id
-                WHERE statename = (:1)      
-                GROUP BY statename, year) e
-            ON p.year = e.year
-            ORDER BY year ASC    
+                (SELECT p.year AS year, total_population AS pop, events
+                FROM
+                    (SELECT sum(p.population) as total_population, l.statename, p.year
+                    FROM michaelrodelo.populations p
+                    INNER JOIN michaelrodelo.location l 
+                    ON p.location_id = l.fips
+                    WHERE statename = (:3)      
+                    GROUP BY l.statename, p.year) p
+                INNER JOIN
+                    (SELECT statename, year, sum(num) AS events
+                    FROM michaelrodelo.location l 
+                        INNER JOIN 
+                        (SELECT yr.year, location_id, COUNT(event_type) AS num
+                        FROM (SELECT DISTINCT EXTRACT(YEAR FROM event_date) AS year 
+                            FROM michaelrodelo.events) yr 
+                        LEFT JOIN michaelrodelo.events e
+                        ON EXTRACT(YEAR FROM e.event_date)=yr.year
+                        GROUP BY yr.year, location_id) e
+                    ON l.fips = e.location_id
+                    WHERE statename = (:4)     
+                    GROUP BY statename, year) e
+                ON p.year = e.year) s2
+            ON s1.year = s2.year   
+            ORDER BY s1.year ASC   
         """
         res = cursor.execute(select_query, query_items)
         for item in res:
             the_chart["yr"].append(item[0])
             the_chart["data1"].append(item[1])
             the_chart["data2"].append(item[2])
-            if len(item) > 3:
-                the_chart["data3"].append(item[3])
-                context["d3hide"] = 0
+            the_chart["data3"].append(item[3])
+            the_chart["data4"].append(item[4])
+            context["d3hide"] = 0
+            context["d4hide"] = 0
         context["the_chart"] = the_chart
     location_data = []
     location_res = cursor.execute(
